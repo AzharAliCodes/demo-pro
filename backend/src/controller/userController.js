b0f45e7407f72d2d7e988df28d4bf1182123f550
Azharuddin Ali <azharuddinali017@gmail.com>
2025-10-24 23:20:53 +0530

    Added validation and hashed password

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -1,4 +1,5 @@
 const { createUser } = require("../model/userModel")
+const bcrypt = require('bcrypt')
 
 const registerUser = async (req, res) => {
   try{
@@ -7,7 +8,29 @@ const registerUser = async (req, res) => {
     if (!name || !password || !phone_number || !email || !profession){
       return res.status(400).json({error: "All fields are required"})
     }
-    const user = await createUser({name, password, phone_number, email, profession})
+
+    if(typeof name !== "string" || name.trim().length <= 3){
+      return res.status(400).json({error:"Name must be at least 2 characters long"})
+    }
+
+    if(password.length < 8){
+      return res.status(400).json({error:"Password must be atleast 8 character"})
+    }
+
+    const phoneRegex = /^[0-9]{10,15}$/;
+    if(!phoneRegex.test(phone_number)){
+      return res.status(400).json({error:"phone number must be  10-15 digits"})
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid email format" });
+    }
+
+    const saltRounds = 10
+    const hashedPassword = await bcrypt.hash(password, saltRounds)
+
+    const user = await createUser({name, password: hashedPassword, phone_number, email, profession})
     res.status(201).json({
            message: "User Created successfully",
            user,
