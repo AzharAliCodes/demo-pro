e13799b88a7fd543caa75a9ca07641e5ef08dcde
Azharuddin Ali <azharuddinali017@gmail.com>
2025-10-24 19:29:14 +0530

    Added configration file to connect postgress

---
+++ backend/src/config/db.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const { pool } = require("pg");
+
+const pool = new pool({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+
+module.exports = pool;
