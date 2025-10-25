2720d1f018b740916018f8a7d2a315c9bc864471
Azharuddin Ali <azharuddinali017@gmail.com>
2025-10-25 21:13:36 +0530

    Completed scan code

--- backend/src/controller/scanController.js
+++ backend/src/controller/scanController.js
@@ -1,5 +1,35 @@
-const {} = require('../model/scanModel')
+const {entry,logScan} = require('../model/scanModel')
+
+const scanEntery = async(req, res) =>{
+  try{
+    const {ticket_id, user_id, scan_type} = req.body
+    const check =await entry(ticket_id)
+        if (!check || check.error) {
+      return res.status(400).json({ error: check?.error || "Invalid ticket" });
+    }
+    await logScan(ticket_id, user_id, scan_type);
+
+    res.status(200).json({ message: "Scan successful", ticket: check });
+  }  catch (err){
+    console.error(err)
+    res.status(500).json({error : "Internal server error"})
+  }
+} 
+
+const scanExit = async(req, res) =>{
+  try{
+    const {ticket_id, scan_type} = req.body
+    await logScan(ticket_id, scan_type);
+
+    res.status(200).json({ message: "Scan successful", ticket: checked });
+  }  catch (err){
+    console.error(err)
+    res.status(500).json({error : "Internal server error"})
+  }
+} 
+
 
 module.exports = {
-
+    scanEntery,
+    scanExit
 }
--- backend/src/model/scanModel.js
+++ backend/src/model/scanModel.js
@@ -1,5 +1,45 @@
 const pool = require('../config/db')
 
-module.exports = {
+const entry = async (ticket_id) => {
+  try{
+      const result = await pool.query(
+      `UPDATE tickets
+       SET is_valid = FALSE
+       WHERE id = $1 AND is_valid = TRUE
+       RETURNING *`,
+      [ticket_id]
+    );
 
+    if (result.rows.length === 0) {
+      return { error: "Invalid ticket" };
+    }
+
+    return { success: true, ticket: result.rows[0] };
+  }catch (err) {
+    console.error(err);
+  }
+}
+
+const logScan = async (ticket_id, user_id, scan_type) => {
+  await pool.query(
+    `INSERT INTO scans (ticket_id, user_id, scan_type) VALUES ($1, $2, $3)`,
+    [ticket_id, user_id, scan_type]
+  );
+};
+
+const excitScan = async (ticket_id, scan_type) => {
+  await pool.query(
+    `UPDATE scans
+  SET scan_type = $2
+  WHERE id = $1;
+  `,
+    [ticket_id, scan_type]
+  );
+};
+
+
+module.exports = {
+entry,
+logScan,
+excitScan
 }
