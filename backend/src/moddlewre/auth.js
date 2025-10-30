8c5c07dbe10949d3e7a02ec5620264de37041d57
AzharAliCodes <azharuddinali017@gmail.com>
2025-10-30 21:27:33 +0530

    Added authenticate code

--- backend/src/moddlewre/auth.js
+++ backend/src/moddlewre/auth.js
@@ -8,8 +8,19 @@ const authenticate = (req, res, next) => {
     if (!authHeader)
         return res.status(401).json({error: "No token provided"})
 
-    const token = authHeader.split("")[1]
+    const token = authHeader.split(" ")[1]
     if(!token)
         return res.status(401).json({error: "Invlid token format"})
+    try{
+        const decoded = jwt.verify(token. process.env.JWT_SECRET)
+        req.user = decoded
+        next()
+    } catch (err){
+        console.error("JWT verfiy error", err.message)
+        return res.status(401).json({error: "Invalid token"})   
+    }
+}
 
+module.exports = {
+    authenticate
 }
