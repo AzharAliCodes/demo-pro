e23fc46eb7541ed62693b60ff0bec6ff74c4b0e8
AzharAliCodes <azharuddinali017@gmail.com>
2025-10-30 21:52:36 +0530

    Added authraztion code

--- backend/src/moddlewre/auth.js
+++ backend/src/moddlewre/auth.js
@@ -12,7 +12,7 @@ const authenticate = (req, res, next) => {
     if(!token)
         return res.status(401).json({error: "Invlid token format"})
     try{
-        const decoded = jwt.verify(token. process.env.JWT_SECRET)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         req.user = decoded
         next()
     } catch (err){
@@ -21,6 +21,21 @@ const authenticate = (req, res, next) => {
     }
 }
 
+const authorize = (allowedRoles) => {
+    return (req, res, next) => {
+        const authHeader = req.headers["authorization"]
+        if (!authHeader)
+           return res.status(401).json({error: "No token provided"})
+        const token = authHeader.split(" ")[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const issuedBy = decoded.role 
+        if(!allowedRoles.includes(issuedBy)){
+            return res.status(403).json({error: "Access denied"})
+        }
+        next()
+    }
+}
 module.exports = {
-    authenticate
+    authenticate,
+    authorize
 }
