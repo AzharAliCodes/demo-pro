4f50f30cf0154d0389d1e729952754daf817d23b
AzharAliCodes <azharuddinali017@gmail.com>
2025-11-01 17:34:52 +0530

    Added Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
+import { Routes, Route } from "react-router-dom"
 import LoginFrom from '../src/components/LoginForm'
+import Home from '../src/components/Home'
+
 
 function App() {
 
   return (
-    <>
-        <LoginFrom/>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+      <Route path="*" element={<h2>page not found</h2>} />
+    </Routes>
   );
 };
 
